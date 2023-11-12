8579e78fa771c41f09a28b64e1f14c7c138b8eae
Anniel <annielfranciscoreyes@gmail.com>
2023-11-12 03:00:18 -0400

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//Layout
+//Layouts
 import { Sidebar } from "./Layouts/Sidebar";
 import { MainLayout } from "./Layouts/MainLayout";
 //Views
