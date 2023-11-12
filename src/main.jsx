fe64cdafc1d69f833bef4091c7b121ff50bcc93d
Anniel <annielfranciscoreyes@gmail.com>
2023-11-12 02:57:03 -0400

    comment

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
+import "aos/dist/aos.css";
 import "./index.css";
 
 AOS.init();
