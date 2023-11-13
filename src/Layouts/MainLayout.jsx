0f1674858dd29fb0ece7427e448e996d99073e1e
Anniel <annielfranciscoreyes@gmail.com>
2023-11-13 00:33:39 -0400

    cambios en MainLayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import "../assets/styles/mainlayout.css";
 import { useLocation } from "react-router-dom";
 import { Login } from "./Login";
 export function MainLayout() {
   const location = useLocation();
-  const [showLogin, setShowLogin] = useState(false);
-  const handleLoginClick = () => {
-    setShowLogin(true);
-  };
+
   return (
     <div className="layout" data-aos="fade-down">
       <div>
