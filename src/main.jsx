42c9db3e70234bbe0c9eaefa159166e7d8ed9757
Anniel <annielfranciscoreyes@gmail.com>
2023-11-26 14:01:04 -0400

    aos

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import AOS from "aos";
 import "./index.css";
 import "animate.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
-AOS.init();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
